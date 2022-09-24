fddd0f706b40c5b160d5aaddf2a352ac76bd431f
jxff-so <jeffso12@hotmail.com>
2022-09-25 01:02:32 +0800

    heh

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function luck() {
+    luck_text = document.getElementById('luck')
+    luck_text.innerHTML = "And you really thought it was gonna work"
+}
+function unluck() {
+    luck_text = document.getElementById('luck')
+    luck_text.innerHTML = "And you gave up just like that"
+}
+function luck_alert() {
+    alert("You have been warned")
+}
